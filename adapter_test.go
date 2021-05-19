743a478660f85d0ebe0fe011106d19d4725b20ab
Hitesh Sethi <hitesh.sethi@rapyuta-robotics.com>
2021-05-20 02:34:49 +0530

    migrated pkg to go mod and upgraded casbin to v2

--- adapter_test.go
+++ adapter_test.go
@@ -1,8 +1,8 @@
 package etcdadapter
 
 import (
-	"github.com/casbin/casbin"
-	"github.com/casbin/casbin/util"
+	"github.com/casbin/casbin/v2"
+	"github.com/casbin/casbin/v2/util"
 	"testing"
 )
 
@@ -21,7 +21,7 @@ func testGetPolicy(t *testing.T, e *casbin.Enforcer, res [][]string) {
 func initPolicy(t *testing.T, pathKey string, etcdEndpoints []string) {
 	// Because the ETCD is empty at first,
 	// so we need to load the policy from the file adapter (.CSV) first.
-	e := casbin.NewEnforcer("examples/rbac_model.conf", "examples/rbac_policy.csv")
+	e, _ := casbin.NewEnforcer("examples/rbac_model.conf", "examples/rbac_policy.csv")
 
 	a := NewAdapter(etcdEndpoints, pathKey)
 	// This is a trick to save the current policy to the ETCD.
@@ -54,7 +54,7 @@ func testSaveLoad(t *testing.T, pathKey string, etcdEndpoints []string) {
 	// Create an adapter and an enforcer.
 	// NewEnforcer() will load the policy automatically.
 	a := NewAdapter(etcdEndpoints, pathKey)
-	e := casbin.NewEnforcer("examples/rbac_model.conf", a)
+	e, _ := casbin.NewEnforcer("examples/rbac_model.conf", a)
 	testGetPolicy(t, e, [][]string{{"alice", "data1", "read"}, {"bob", "data2", "write"}, {"data2_admin", "data2", "read"}, {"data2_admin", "data2", "write"}})
 }
 
@@ -68,7 +68,7 @@ func testAutoSave(t *testing.T, pathKey string, etcdEndpoints []string) {
 	// Create an adapter and an enforcer.
 	// NewEnforcer() will load the policy automatically.
 	a := NewAdapter(etcdEndpoints, pathKey)
-	e := casbin.NewEnforcer("examples/rbac_model.conf", a)
+	e, _ := casbin.NewEnforcer("examples/rbac_model.conf", a)
 
 	// AutoSave is enabled by default.
 	// Now we disable it.
